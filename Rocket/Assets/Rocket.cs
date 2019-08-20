b1b6490f5426d753756684ab32cfe06609421cef
B533P-13\533 <Mentxllityy@gmail.com>
2019-08-20 11:30:03 -0400

    Fini

---
+++ Rocket/Assets/Rocket.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+    Rigidbody rbd;
+
+  
+    public  int TrustFACTOR = 500;
+    public  int RelationFactor = 5000;
+    // Use this for initialization
+    void Start () {
+        rbd = gameObject.GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetAxis("Jump") != 0)
+        {
+            rbd.AddRelativeForce(new Vector3(0, Input.GetAxis("Jump")* TrustFACTOR, 0));
+        }
+
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+         
+            rbd.AddForceAtPosition(new Vector3(Input.GetAxis("Horizontal")* RelationFactor, 0, 0), new Vector3(0,50, 0) );
+        }
+    }
+}
